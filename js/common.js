04c371c7b39d81921e6a60121f3839a9522e83c5
hwkims <hwkims@naver.com>
2024-08-23 16:08:16 +0900

    1

--- js/common.js
+++ js/common.js
@@ -4,7 +4,7 @@ $(function () {
 	var swiper2 = new Swiper(".fullpage", {
 		direction: "vertical",
 		slidesPerView: 1,
- 		spaceBetween: 60, 
+ 		spaceBetween: 200, 
 		mousewheel: true,
 		pagination: {
 			el: ".swiper-pagination",
